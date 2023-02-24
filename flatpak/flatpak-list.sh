4a1f74f3e082d2688345ba67cad79937981986e0
arega24 <dfbarega@gmail.com>
2023-02-24 15:18:46 +0000

    flatpak update

--- flatpak/flatpak-list.sh
+++ flatpak/flatpak-list.sh
@@ -4,9 +4,6 @@
 
 flatpak install flathub com.github.tchx84.Flatseal &
 
-flatpak install flathub com.discordapp.Discord &
-flatpak install flathub com.logseq.Logseq &
-flatpak install flathub org.onlyoffice.desktopeditors &
-flatpak install flathub io.gitlab.librewolf-community
+flatpak install flathub com.discordapp.Discord
 
 
