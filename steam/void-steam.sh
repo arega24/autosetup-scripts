eeaf5a09126ea086de0f89558ef4dc1868577ed6
arega24 <99811425+arega24@users.noreply.github.com>
2023-02-26 21:15:52 +0000

    Update void-steam.sh

--- steam/void-steam.sh
+++ steam/void-steam.sh
@@ -6,7 +6,7 @@ sudo xbps-install -Sy void-repo-multilib-nonfree
 sudo xbps-install -S
 
 sudo xbps-install libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit
-sudo xbps-install steam
+sudo xbps-install steam xboxdrv
 
 
 
