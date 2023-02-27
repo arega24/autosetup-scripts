53440c1589b19794e80447982ab0ca79d4802cc5
arega24 <99811425+arega24@users.noreply.github.com>
2023-02-27 21:03:38 +0000

    wine

--- steam/void-steam.sh
+++ steam/void-steam.sh
@@ -5,6 +5,9 @@ sudo xbps-install -Sy void-repo-multilib
 sudo xbps-install -Sy void-repo-multilib-nonfree
 sudo xbps-install -S
 
+#wine
+sudo xbps-install wine wine-32bit
+
 sudo xbps-install libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit
 sudo xbps-install steam xboxdrv
 
