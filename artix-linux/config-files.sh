5174380ee84d3948b6b460c737460ddf9da9819d
arega24 <99811425+arega24@users.noreply.github.com>
2023-07-21 17:10:07 +0000

    Update config-files.sh

--- artix-linux/config-files.sh
+++ artix-linux/config-files.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-cd &
-git clone https://github.com/arega24/suckless-dwm &
-git clone https://github.com/arega24/dwm-dots &
-git clone https://github.com/arega24/Neovim &
-mkdir .config &
-cp -r suckless-dwm/dwm-6.4-patched .config/ &
-cp -r suckless-dwm/slstatus .config/ &
+cd 
+git clone https://github.com/arega24/suckless-dwm 
+git clone https://github.com/arega24/dwm-dots 
+git clone https://github.com/arega24/Neovim 
+mkdir .config 
+cp -r suckless-dwm/dwm-6.4-patched .config/ 
+cp -r suckless-dwm/slstatus .config/ 
 
-mkdir ~/.config/nvim &
-cp -r Neovim/* ~/.config/nvim/ &
+mkdir ~/.config/nvim 
+cp -r Neovim/* ~/.config/nvim/ 
 
-cp -r dwm-dots/dunst/ ~/.config/ &
-cp -r dwm-dots/lf/ ~/.config/ &
-cp -r dwm-dots/picom/ ~/.config/ &
-cp -r dwm-dots/rofi/ ~/.config/ &
-cp -r dwm-dots/scripts/ ~/.config/ &
-cp -r dwm-dots/start_pages/ ~/.config/ &
-cp -r dwm-dots/sxiv/ ~/.config/ &
-cp -r dwm-dots/wal/ ~/.config/ &
-cp -r dwm-dots/zathura/ ~/.config/ &
+cp -r dwm-dots/dunst/ ~/.config/ 
+cp -r dwm-dots/lf/ ~/.config/ 
+cp -r dwm-dots/picom/ ~/.config/
+cp -r dwm-dots/rofi/ ~/.config/ 
+cp -r dwm-dots/scripts/ ~/.config/ 
+cp -r dwm-dots/start_pages/ ~/.config/ 
+cp -r dwm-dots/sxiv/ ~/.config/ 
+cp -r dwm-dots/wal/ ~/.config/ 
+cp -r dwm-dots/zathura/ ~/.config/ 
 
-cp dwm-dots/Xresources ~/.Xresources & 
-cp dwm-dots/xinitrc ~/.xinitrc &
+cp dwm-dots/Xresources ~/.Xresources &
+cp dwm-dots/xinitrc ~/.xinitrc 
 cp dwm-dots/bashrc ~/.bashrc 
