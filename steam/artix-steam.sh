a6487660a3ca06cd1bc91354545f64e8ecd3af9d
arega24 <99811425+arega24@users.noreply.github.com>
2023-07-23 19:51:04 +0000

    Update artix-steam.sh

--- steam/artix-steam.sh
+++ steam/artix-steam.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
-# Edit /etc/pacman.conf and enable Artix [lib32] and Arch [multilib] repositories
+# Edit /etc/pacman.conf and enable Artix [lib32] and Arch [multilib] repositories. Need to enabe [universe] 
 # then upgrade the system -> pacman -Syu
 
 # steam
 sudo pacman -S steam ttf-liberation
 # wine
-yay -Ss wine-stable
+yay -S wine
 # lutris
-sudo pacman -S lutris
+sudo pacman -S lutris 
 # controler and wheel suport
 
 
