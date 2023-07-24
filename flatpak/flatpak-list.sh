bab9a0ddc841e2edb4540261f0cb6ed8a5ec693f
arega24 <99811425+arega24@users.noreply.github.com>
2023-07-24 00:00:27 +0000

    Update flatpak-list.sh

--- flatpak/flatpak-list.sh
+++ flatpak/flatpak-list.sh
@@ -3,6 +3,7 @@
 
 sudo flatpak install flathub com.github.tchx84.Flatseal
 
-sudo flatpak install flathub com.discordapp.Discord
-#sudo flatpak install flathub com.logseq.Logseq
+sudo flatpak install flathub de.shorsh.discord-screenaudio
+# sudo flatpak install flathub com.discordapp.Discord
+# sudo flatpak install flathub com.logseq.Logseq
 
