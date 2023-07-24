d5ca92d8c7821f01a96a180e7460a12646d1bc65
arega24 <99811425+arega24@users.noreply.github.com>
2023-07-24 00:04:03 +0000

    Update flatpak-list.sh

--- flatpak/flatpak-list.sh
+++ flatpak/flatpak-list.sh
@@ -3,6 +3,7 @@
 flatpak install flathub com.github.tchx84.Flatseal
 flatpak install flathub org.clementine_player.Clementine
 
+# Discord with audio on screen sharing
 flatpak install flathub de.shorsh.discord-screenaudio
 # sudo flatpak install flathub com.discordapp.Discord
 # sudo flatpak install flathub com.logseq.Logseq
