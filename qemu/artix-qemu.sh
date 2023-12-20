1a34ba72f1a32e4859411c0bd9a61b7184248c02
arega24 <99811425+arega24@users.noreply.github.com>
2023-12-20 13:49:25 +0000

    Update artix-qemu.sh

--- qemu/artix-qemu.sh
+++ qemu/artix-qemu.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env sh
-# make file from scratch
+
+sudo pacman -S qemu-full virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat libvirt-runit
+sudo ln -s /etc/runit/sv/libvirtd/ /run/runit/service/
